c6904ef3cce55c274e083764ad722ff360153cad
kgar <khaled.garbaya@gameduell.com>
2015-05-22 14:40:20 +0200

    Added Unit test

---
+++ tests/source/MainTest.hx
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2003-2015 GameDuell GmbH, All Rights Reserved
+ * This document is strictly confidential and sole property of GameDuell GmbH, Berlin, Germany
+ */
+
+package;
+
+import unittest.TestRunner;
+import unittest.implementations.TestHTTPLogger;
+import unittest.implementations.TestJUnitLogger;
+import unittest.implementations.TestSimpleLogger;
+
+import duell.DuellKit;
+
+import test.SoundTest;
+/**
+ * @author kgar
+ */
+class MainTest
+{
+
+    private var r: TestRunner;
+
+    public function new()
+    {
+        DuellKit.initialize(startAfterDuellIsInitialized);
+    }
+
+    private function startAfterDuellIsInitialized(): Void
+    {
+        r = new TestRunner(testComplete, DuellKit.instance().onError);
+        r.add(new SoundTest());
+
+        #if test
+
+        #if jenkins
+        r.addLogger(new TestHTTPLogger(new TestJUnitLogger()));
+        #else
+        r.addLogger(new TestHTTPLogger(new TestSimpleLogger()));
+        #end
+
+        #else
+        r.addLogger(new TestSimpleLogger());
+        #end
+
+        r.run();
+    }
+
+    private function testComplete(): Void
+    {
+        trace(r.result);
+    }
+
+    /// MAIN
+    static var _main: Main;
+
+    static function main(): Void
+    {
+        _main = new Main();
+    }
+}
